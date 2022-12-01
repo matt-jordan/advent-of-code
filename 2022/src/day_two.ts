341033c7c9d7d8316b405c7e551125602563a4a3
Matt Jordan <mcjordan81@gmail.com>
2022-12-01 06:53:45 -0800

    Add day two, which isn't day two. It's just the second gold star. Whoops.

---
+++ 2022/src/day_two.ts
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
+
+const directory: string = path.resolve();
+const filepath: string = `${directory}/2022/src/input`;
+
+const file = readline.createInterface({
+    input: fs.createReadStream(filepath),
+});
+
+let current: number = 0;
+const calories: number[] = [];
+
+file.on('line', (line: string) => {
+    if (line === '') {
+        calories.push(current);
+        current = 0;
+    } else {
+        current += Number(line);
+    }
+});
+
+file.on('close', () => {
+    calories.sort((a, b) => { return a - b; }).reverse();
+    const top_three: number = calories[0] + calories[1] + calories[2];
+    console.log(`The top three elves are carrying: ${top_three} calories`);
+});
